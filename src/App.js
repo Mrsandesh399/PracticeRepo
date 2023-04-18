e90c23bf0e49d672b46ec40aaabeaecc3c410a34
Mrsandesh399 <sandeshumathe123@gmail.com>
2023-04-18 18:41:53 +0530

    implement side effects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginData = localStorage.getItem('isLoggedIn');
+    if (checkLoginData === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn', '1');
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div>
-      <h1>hey Sandy!</h1>
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
